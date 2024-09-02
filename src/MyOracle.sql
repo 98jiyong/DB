f75ef28a33d790efc7bb70fb7c20284fd8f8e2f9
Jeong Jiyong <98jiyong@gmail.com>
2024-09-02 19:06:10 +0900

    20240902_DB_집에서복습

--- src/MyOracle.sql
+++ src/MyOracle.sql
@@ -1,3 +1,4 @@
+--테이블 생성
 create table users(
 no int primary key,
 name varchar2(10),
@@ -6,7 +7,7 @@ point number,
 grade varchar2(1),
 jumin varchar2(8));
 
-/* 테이블 구조 확인 */
+--테이블 구조 확인
 desc users;
 
 insert into users values (1,'kimsu','suwon',99.12,1,'820405-1');
@@ -22,10 +23,10 @@ insert into users values (10,'kimmi','incheon',79.92,1,'810505-1');
 insert into users values (11,'ryusu','seoul',85.32,4,'861205-2');
 insert into users values (12,'Gosu',null,82.13,4,'810715-1');
 
-/* 레코드 확인 */
+--레코드 확인
 select * from users;
 
-/* 1번 ~ 12번 목표문제 */
+--1 ~ 12 목표문제
 select name 이름, point 점수, substr(jumin,1,6) 생년월일 from users;
 select name 이름, addr 주소, point 점수 from users where point >= 80;
 select name 이름, addr 주소, point 점수 from users where name like 'kim%';
@@ -39,7 +40,7 @@ select no 번호, name 이름, grade 학년, point 점수 from users order by grade asc,
 select name 이름, point 점수, point-10 보정한점수 from users where point-10 >= 80;
 select (name || ' '  || addr || ' ' || grade) "이름 주소 학년" from users where grade = 2;
 
-/* 13번 ~ 23번 추가문제 */
+--13 ~ 23 추가문제
 select count(*) from users;
 select count(*) from users where grade = 1;
 select LOWER(name) 소문자이름, grade 학년 from users;
@@ -52,7 +53,7 @@ select avg(point) from users where grade = 3;
 select MAX(point) from users;
 select MIN(point) from users where grade = 2;
 
-/* 24번 ~ 27번 못푸는 문제 */
+--24 ~ 27 못푸는 문제 
 /* trim 함수는 반복적인 문자를 제거하기 때문에 9번과 10번의 incheon에 n이 반복되어서 'in'이 아닌 'i'만 출력
 select ltrim(addr,substr(addr,1,2)) "앞두글자를 제외한 글자", rtrim(addr,ltrim(addr,substr(addr,1,2))) "앞에 값을 rtrim 사용" from users;
 select concat(rtrim(addr,ltrim(addr,substr(addr,1,2))),'*') "@@*" from users;
@@ -62,10 +63,11 @@ select '*' || name || '*' from users;
 select concat(substr(jumin,1,2),'년') || concat(substr(jumin,3,2),'월') || concat(substr(jumin,5,2),'일') 생년월일 from users;
 select decode(substr(jumin,instr(jumin,'-',1)+1,1),1,'남',2,'여') 성별 from users;
 
-/* 28번 ~ 30번 추가문제 */
+--28 ~ 30 추가문제
 select name 이름, jumin 주민번호, 124-substr(jumin,1,2) 나이 from users;
 select name 이름, rpad(substr(name,1,1),length(name),'*') "a***" from users;
 select name 이름, grade 학년, decode(grade,1,name||'*',2,name||'%',3,name||'#',4,name||'!') 조건 from users;
+
 --다른풀이
 select name 이름, grade 학년, case 
 when grade = 1 then name || '*'
